d54aefc01b809f4dd3a7913bb59d61f8f8ba1d2e
Andrea Velásquez <a.velasquez12@uniandes.edu.co>
2017-04-07 19:18:49 -0500

    divide data

--- ej2.py
+++ ej2.py
@@ -11,20 +11,32 @@ data = pd.read_csv('http://www-bcf.usc.edu/~gareth/ISL/Credit.csv', index_col=0)
 X=data[['Income', 'Limit', 'Rating', 'Cards', 'Age', 'Education', 'Gender']]
 Y=data['Balance']
 
-X_N=X[['Income', 'Limit', 'Rating', 'Cards', 'Age', 'Education']]
+X_N=X['Income']
 X_G=X['Gender'].values
 trainG=[]
 
-linreg=linear_model.LinearRegression()
-linreg.fit(X_N,Y)
-print(linreg.coef_)
+#linreg=linear_model.LinearRegression()
+#linreg.fit(X_N,Y)
+#print(linreg.coef_)
 
 for i in range(len(X_G)):
     if X_G[i]==' Male':
         trainG.append(0)
     else:
         trainG.append(1)
-print(trainG)
+
+income = np.asarray(X_N.values)
+gender = np.asarray(trainG)
+
+male_incomes = []
+female_incomes = []
+for i in range(len(income)):
+    if gender[i] == 0:
+        male_incomes.append(income[i])
+    if gender[i] == 1:
+        female_incomes.append(income[i])
+
+
 
 
 
